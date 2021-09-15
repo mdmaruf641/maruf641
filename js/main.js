c8747eec7770ccde70c43cd7ec283735098f7f2e
mdmaruf641 <mdmarufmoni50@gmail.com>
2021-09-15 16:30:23 +0600

    sticky menu added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 $(function(){
+	 //=====bottom to top button=====//
+	 $(window).scroll(function(){
+
+		var scrolling = $(this).scrollTop();
+
+		if(scrolling > 50){
+				$(".bottom-to-top").fadeIn(500);
+		}
+		else{
+				$(".bottom-to-top").fadeOut(500); 
+		}
+		// sticky menu //
+		if(scrolling > 100){
+				$("#nav").addClass("sticky-bg");
+		}
+		else{
+				$("#nav").removeClass("sticky-bg");
+		}
+	});
+
+ $(".bottom-to-top").click(function(){
+		$('html, body').animate({
+				scrollTop:0
+		}, 2000);
+ });      
+
+	// types js
 	$(".typed").typed({
 		strings: ["Designer.", "Developer.", "Professional Coder."],
 		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
